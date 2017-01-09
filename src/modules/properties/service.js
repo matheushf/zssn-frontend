a66a46b89156facac59ec3c04f9ef699cf93e687
Matheus Victor <hffmatheus@gmail.com>
2017-01-09 15:16:11 -0200

    separate url

--- src/modules/properties/service.js
+++ src/modules/properties/service.js
@@ -1,14 +1,17 @@
 angular.module('appZssn')
     .factory('PropertiesApi', function ($http, $resource) {
-        return $resource('http://zssn-backend-example.herokuapp.com/api/people/:person_id', null, {
+
+        var url = 'http://zssn-backend-example.herokuapp.com/api/people/';
+
+        return $resource(url + ':person_id', null, {
             trade: {
                 method: 'POST',
-                url: 'http://zssn-backend-example.herokuapp.com/api/people/:person_id/properties/trade_item'
+                url: url + ':person_id/properties/trade_item'
             },
             fetch: {
                 method: 'GET',
                 isArray: true,
-                url: 'http://zssn-backend-example.herokuapp.com/api/people/:person_id/properties'
+                url: url + ':person_id/properties'
             }
         })
     });
